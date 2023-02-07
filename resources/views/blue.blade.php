02fb35b5e8f2df80a65397d0cec3b5118646cabf
Sukeb <sukebushido@gmail.com>
2023-02-07 14:37:38 +0100

    Organisation des réponses dans la thread

--- resources/views/blue.blade.php
+++ resources/views/blue.blade.php
@@ -1,17 +1,23 @@
-@extends("layout.app")
+@extends('layout.app')
 
-@section("content")
+@section('content')
     <div class="test main">
         <p>I'm a blue board</p>
         @foreach ($posts as $post)
-        {{-- {{dd($post->thread_id)}} --}}
-            <div class="container {{$post->id == $post->thread_id ? "main" : "reply"}}">
-                <p>id : {{$post->id}}</p>
-                <p>title : {{$post->title}}</p>
-                <p>author : {{$post->author}}</p>
-                <p>créé le : {{$post->created_at}}</p>
-                <p>OP : {{$post->OP}}</p>
-                <p>Contenu : {{$post->content}}</p>
+            {{-- {{dd($post->thread_id)}} --}}
+            <div class="container">
+                {!! !$post->OP ? "<div class='siderarrows'>>></div>" : "" !!}
+                <div class="post {{ $post->id == $post->thread_id ? 'main' : 'reply' }}">
+                    <div class="title-container">
+                        <span class="title">{{ $post->title }}</span>
+                        <span class="author">{{ $post->author }}</span>
+                        <span class="created-at">{{ $post->created_at }}</span>
+                        <span class="id">No.{{ $post->id }}</span>
+                    </div>
+                    <div class="content-container">
+                        <p class="content">{{ $post->content }}</p>
+                    </div>
+                </div>
             </div>
         @endforeach
     </div>
