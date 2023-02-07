ab53e87e565645bda42c506274f3ecc895d86e8e
Sukeb <sukebushido@gmail.com>
2023-02-07 10:24:28 +0100

    Layout basique

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blue', function() {
+    return view('blue');
+});
+
+Route::get('/red', function() {
+    return view('red');
+});
