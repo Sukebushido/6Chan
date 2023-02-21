451a581f6f26812f77495cae807cb86e35e4387e
Sukeb <sukebushido@gmail.com>
2023-02-21 12:43:59 +0100

    Changements mineurs, ajout de <a> dans le post.component.blade.php

--- resources/views/components/post-component.blade.php
+++ resources/views/components/post-component.blade.php
@@ -1,11 +1,16 @@
 <div class="container">
-    {!! !$post->OP ? "<div class='sidearrows'>>></div>" : "" !!}
+    {!! !$post->OP ? "<div class='sidearrows'>>></div>" : '' !!}
     <div class="post {{ $post->id == $post->thread_id ? 'main' : 'reply' }}" id="p{{ $post->id }}">
         <div class="title-container">
             <span class="title">{{ $post->title }}</span>
             <span class="author">{{ $post->author }}</span>
             <span class="created-at">{{ $post->created_at }}</span>
-            <span class="id">No.{{ $post->id }}</span>
+            <span>
+                <a href="#" class="link" title="Link to this post">No.</a>
+                <a href="#" class="id" title="Reply to this post">{{ $post->id }}</a>
+            </span>
+            {{-- Need to fix later, linkin to thread  --}}
+            {!! $post->OP ? '[<a href="#">Reply</a>]' : '' !!}
             <i class="fa-solid fa-caret-right"></i>
         </div>
         <div class="content-container">
