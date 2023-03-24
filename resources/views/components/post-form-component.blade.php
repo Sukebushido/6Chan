087f1f15ef9e39343ca5aa7ec828a1e1da52e96b
Sukeb <sukebushido@gmail.com>
2023-03-24 14:51:09 +0100

    Ajout d'un composant suffisamment satisfaisant niveau front

---
+++ resources/views/components/post-form-component.blade.php
@@ -0,0 +1,58 @@
+<div class="d-flex f-column f-center">
+    <p>Create new thread</p>
+    <form action="">
+        <table>
+            <tbody>
+                <tr data-type="name">
+                    <td class="label">
+                        Name
+                    </td>
+                    <td>
+                        <input type="text" name="name" id="name" tabindex="3" placeholder="Anonymous">
+                    </td>
+                </tr>
+                <tr data-type="options">
+                    <td class="label">
+                        Options
+                    </td>
+                    <td>
+                        <input type="text" name="options" id="options"tabindex="3" >
+                    </td>
+                </tr>
+                <tr data-type="subject">
+                    <td class="label">
+                        Subject
+                    </td>
+                    <td>
+                        <input type="text" name="subject" id="subject" tabindex="3" >
+                        <input type="submit" value="Post" tabindex="3">
+                    </td>
+                </tr>
+                <tr data-type="comment">
+                    <td class="label">
+                        Comment
+                    </td>
+                    <td>
+                        <textarea name="comment" cols="48" rows="4" wrap="soft" tabindex="4" id="comment"></textarea>
+                    </td>
+                </tr>
+                <tr data-type="verification">
+                    <td class="label">
+                        Verification
+                    </td>
+                    <td>
+                        <input type="text" name="verification" id="verification" placeholder="Coming Soon">
+                    </td>
+                </tr>
+                <tr data-type="file">
+                    <td class="label">
+                        File
+                    </td>
+                    <td>
+                        <input type="file" name="file" id="file">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </form>
+</div>
